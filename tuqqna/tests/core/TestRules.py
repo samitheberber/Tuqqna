bb85bb587a9d63c5294bf77a9d259e3ec9e34209
Sami Saada <sami.saada@gmail.com>
2009-10-07 00:14:29 +0300

    Updated game core. - Added core game - Changed rules to be more accuire. - Changed board to do only board relevant subjects.

--- tuqqna/tests/core/TestRules.py
+++ tuqqna/tests/core/TestRules.py
@@ -17,126 +17,110 @@ from tuqqna.core.rules import RulesCross
 
 class TestHorizontalRules(unittest.TestCase):
 
-    def setUp(self):
-        self.player1 = Player("Player 1")
-        self.player2 = Player("Player 2")
-
     def test_no_win_when_only_one_button_on_field(self):
-        buttons = [(self.player1, Button(0,0))]
-        self.assertFalse(RulesHorizontal.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0)]
+        self.assertFalse(RulesHorizontal.check(0, 0, buttons))
 
     def test_win_on_four_row_last_at_beginning(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,0)), (self.player1, Button(2,0)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesHorizontal.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0), Button(1,0), Button(2,0), Button(3,0)]
+        self.assertTrue(RulesHorizontal.check(0, 0, buttons))
 
     def test_win_on_four_row_last_at_end(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,0)), (self.player1, Button(2,0)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesHorizontal.check(self.player1, 0, 3, buttons))
+        buttons = [Button(0,0), Button(1,0), Button(2,0), Button(3,0)]
+        self.assertTrue(RulesHorizontal.check(0, 3, buttons))
 
     def test_win_on_four_row_last_at_center(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,0)), (self.player1, Button(2,0)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesHorizontal.check(self.player1, 0, 2, buttons))
+        buttons = [Button(0,0), Button(1,0), Button(2,0), Button(3,0)]
+        self.assertTrue(RulesHorizontal.check(0, 2, buttons))
 
     def test_no_win_on_four_row_last_at_center(self):
-        buttons = [(self.player1, Button(0,0)), (self.player2, Button(1,0)), (self.player1, Button(2,0)), (self.player1, Button(3,0))]
-        self.assertFalse(RulesHorizontal.check(self.player1, 0, 2, buttons))
+        buttons = [Button(0,0), Button(2,0), Button(3,0)]
+        self.assertFalse(RulesHorizontal.check(0, 2, buttons))
 
     def test_win_on_four_row_not_on_borders_last_at_center(self):
-        buttons = [(self.player1, Button(4,1)), (self.player1, Button(1,1)), (self.player1, Button(2,1)), (self.player1, Button(3,1))]
-        self.assertTrue(RulesHorizontal.check(self.player1, 1, 3, buttons))
+        buttons = [Button(1,1), Button(2,1), Button(3,1), Button(4,1)]
+        self.assertTrue(RulesHorizontal.check(1, 3, buttons))
 
 
 class TestVerticalRules(unittest.TestCase):
 
-    def setUp(self):
-        self.player1 = Player("Player 1")
-        self.player2 = Player("Player 2")
-
     def test_no_win_when_only_one_button_on_field(self):
-        buttons = [(self.player1, Button(0,0))]
-        self.assertFalse(RulesVertical.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0)]
+        self.assertFalse(RulesVertical.check(0, 0, buttons))
 
     def test_win_on_four_column(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(0,1)), (self.player1, Button(0,2)), (self.player1, Button(0,3))]
-        self.assertTrue(RulesVertical.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0), Button(0,1), Button(0,2), Button(0,3)]
+        self.assertTrue(RulesVertical.check(0, 0, buttons))
 
     def test_no_win_on_four_column(self):
-        buttons = [(self.player1, Button(0,0)), (self.player2, Button(0,1)), (self.player1, Button(0,2)), (self.player1, Button(0,3))]
-        self.assertFalse(RulesVertical.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0), Button(0,2), Button(0,3)]
+        self.assertFalse(RulesVertical.check(0, 0, buttons))
 
     def test_win_on_four_column_not_on_borders_last(self):
-        buttons = [(self.player1, Button(1,1)), (self.player1, Button(1,2)), (self.player1, Button(1,3)), (self.player1, Button(1,4))]
-        self.assertTrue(RulesVertical.check(self.player1, 1, 1, buttons))
+        buttons = [Button(1,1), Button(1,2), Button(1,3), Button(1,4)]
+        self.assertTrue(RulesVertical.check(1, 1, buttons))
 
 
 class TestCrossRules(unittest.TestCase):
 
-    def setUp(self):
-        self.player1 = Player("Player 1")
-        self.player2 = Player("Player 2")
-
     def test_no_win_when_only_one_button_on_field(self):
-        buttons = [(self.player1, Button(0,0))]
-        self.assertFalse(RulesCross.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0)]
+        self.assertFalse(RulesCross.check(0, 0, buttons))
 
     def test_win_on_four_row_ascending_last_at_beginning(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,1)), (self.player1, Button(2,2)), (self.player1, Button(3,3))]
-        self.assertTrue(RulesCross.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0), Button(1,1), Button(2,2), Button(3,3)]
+        self.assertTrue(RulesCross.check(0, 0, buttons))
 
     def test_win_on_four_row_ascending_last_at_end(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,1)), (self.player1, Button(2,2)), (self.player1, Button(3,3))]
-        self.assertTrue(RulesCross.check(self.player1, 3, 3, buttons))
+        buttons = [Button(0,0), Button(1,1), Button(2,2), Button(3,3)]
+        self.assertTrue(RulesCross.check(3, 3, buttons))
 
     def test_win_on_four_row_ascending_last_at_center(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,1)), (self.player1, Button(2,2)), (self.player1, Button(3,3))]
-        self.assertTrue(RulesCross.check(self.player1, 2, 2, buttons))
+        buttons = [Button(0,0), Button(1,1), Button(2,2), Button(3,3)]
+        self.assertTrue(RulesCross.check(2, 2, buttons))
 
     def test_no_win_on_four_row_ascending_last_at_center(self):
-        buttons = [(self.player1, Button(0,0)), (self.player2, Button(1,1)), (self.player1, Button(2,2)), (self.player1, Button(3,3))]
-        self.assertFalse(RulesCross.check(self.player1, 2, 2, buttons))
+        buttons = [Button(0,0), Button(2,2), Button(3,3)]
+        self.assertFalse(RulesCross.check(2, 2, buttons))
 
     def test_win_on_four_row_descending_last_at_beginning(self):
-        buttons = [(self.player1, Button(0,3)), (self.player1, Button(1,2)), (self.player1, Button(2,1)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesCross.check(self.player1, 3, 0, buttons))
+        buttons = [Button(0,3), Button(1,2), Button(2,1), Button(3,0)]
+        self.assertTrue(RulesCross.check(3, 0, buttons))
 
     def test_win_on_four_row_descending_last_at_end(self):
-        buttons = [(self.player1, Button(0,3)), (self.player1, Button(1,2)), (self.player1, Button(2,1)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesCross.check(self.player1, 0, 3, buttons))
+        buttons = [Button(0,3), Button(1,2), Button(2,1), Button(3,0)]
+        self.assertTrue(RulesCross.check(0, 3, buttons))
 
     def test_win_on_four_row_descending_last_at_center(self):
-        buttons = [(self.player1, Button(0,3)), (self.player1, Button(1,2)), (self.player1, Button(2,1)), (self.player1, Button(3,0))]
-        self.assertTrue(RulesCross.check(self.player1, 1, 2, buttons))
+        buttons = [Button(0,3), Button(1,2), Button(2,1), Button(3,0)]
+        self.assertTrue(RulesCross.check(1, 2, buttons))
 
     def test_no_win_on_four_row_descending_last_at_center(self):
-        buttons = [(self.player1, Button(0,3)), (self.player2, Button(1,2)), (self.player1, Button(2,1)), (self.player1, Button(3,0))]
-        self.assertFalse(RulesCross.check(self.player1, 1, 2, buttons))
+        buttons = [Button(0,3), Button(2,1), Button(3,0)]
+        self.assertFalse(RulesCross.check(1, 2, buttons))
 
 
 class TestRules(unittest.TestCase):
 
-    def setUp(self):
-        self.player1 = Player("Player 1")
-        self.player2 = Player("Player 2")
-
     def test_no_win_on_any_rule(self):
-        buttons = [(self.player1, Button(0,0))]
-        self.assertFalse(Rules.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0)]
+        self.assertFalse(Rules.check(0, 0, buttons))
 
     def test_win_on_horizontal_rule(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,0)), (self.player1, Button(2,0)), (self.player1, Button(3,0))]
-        self.assertTrue(Rules.check(self.player1, 0, 2, buttons))
+        buttons = [Button(0,0), Button(1,0), Button(2,0), Button(3,0)]
+        self.assertTrue(Rules.check(0, 2, buttons))
 
     def test_win_on_vertical_rule(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(0,1)), (self.player1, Button(0,2)), (self.player1, Button(0,3))]
-        self.assertTrue(Rules.check(self.player1, 0, 0, buttons))
+        buttons = [Button(0,0), Button(0,1), Button(0,2), Button(0,3)]
+        self.assertTrue(Rules.check(0, 0, buttons))
 
     def test_win_on_asscending_cross_rule(self):
-        buttons = [(self.player1, Button(0,0)), (self.player1, Button(1,1)), (self.player1, Button(2,2)), (self.player1, Button(3,3))]
-        self.assertTrue(Rules.check(self.player1, 2, 2, buttons))
+        buttons = [Button(0,0), Button(1,1), Button(2,2), Button(3,3)]
+        self.assertTrue(Rules.check(2, 2, buttons))
 
     def test_win_on_descending_cross_rule(self):
-        buttons = [(self.player1, Button(0,3)), (self.player1, Button(1,2)), (self.player1, Button(2,1)), (self.player1, Button(3,0))]
-        self.assertTrue(Rules.check(self.player1, 1, 2, buttons))
+        buttons = [Button(0,3), Button(1,2), Button(2,1), Button(3,0)]
+        self.assertTrue(Rules.check(1, 2, buttons))
 
 
 def suite():
